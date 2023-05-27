f22d3f864ad6d8829727f061e47cceced883f4d4
Mickey0002 <127755383+Mickey0002@users.noreply.github.com>
2023-05-27 10:18:11 +0300

    changes

--- Control.js
+++ Control.js
@@ -1,19 +1,19 @@
 // window.location.href = URL;
 // window.location.replace(URL);
-let loginForm= document.getElementById("loginForm");
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
-let user = document.getElementById("username").value;
-let pass = document.getElementById("Pass").value;
-function Move(){
-if(user === "mickey" && pass === "7181"){
-    location.replace('/client.html');
-    alert("tmam");
+// let loginForm= document.getElementById("loginForm");
+// loginForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+// })
+// let user = document.getElementById("username").value;
+// let pass = document.getElementById("Pass").value;
+// function Move(){
+// if(user === "mickey" && pass === "7181"){
+//     location.replace('/client.html');
+//     alert("tmam");
   
-}
-if (user === "" || pass === ""){
-    alert("wrong") ;
-}
+// }
+// if (user === "" || pass === ""){
+//     alert("wrong") ;
+// }
 
-}
+// }
